5f68779533f1ccb63edd851d52d9a5dc3ceae640
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-06-25 22:02:58 +0200

    setup empty gdx project with desktop gui

---
+++ core/src/com/mygdx/chess/GuiParams.java
@@ -0,0 +1,6 @@
+package com.mygdx.chess;
+
+public class GuiParams {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
+}
---
+++ desktop/src/com/mygdx/chess/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.chess;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("Chess");
+		config.setWindowedMode(GuiParams.WIDTH, GuiParams.HEIGHT);
+        new Lwjgl3Application(new Chess(), config);
+    }
+}
