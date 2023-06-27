f74a14776cee94090e226b4b52ebd56afc27349a
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-06-27 16:07:48 +0200

    setup gdx project: - added Screen (GameScreen) and set it - in 'Chess' class, removed 'Texture' - in 'Chess' class, added 'BitmapFont' - in 'Chess' class, set Screen (GameScreen) - changed width and height of displaying window

--- core/src/com/mygdx/chess/Chess.java
+++ core/src/com/mygdx/chess/Chess.java
@@ -1,31 +1,36 @@
 package com.mygdx.chess;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Chess extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+public class Chess extends Game {
+    private SpriteBatch batch;
+    private BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        font = new BitmapFont();
+        this.setScreen(new GameScreen(this));
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        super.render();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        font.dispose();
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public BitmapFont getFont() {
+        return font;
     }
 }
---
+++ core/src/com/mygdx/chess/GameScreen.java
@@ -0,0 +1,63 @@
+package com.mygdx.chess;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class GameScreen implements Screen {
+    private final Chess game;
+    private final OrthographicCamera camera;
+    private final Stage stage;
+
+    public GameScreen(Chess chess) {
+        this.game = chess;
+
+        //przygotowanie elementów graficznych
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, GuiParams.WIDTH, GuiParams.HEIGHT);
+        stage = new Stage(new ScreenViewport(), game.getBatch());
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.WHITE);
+        camera.update();
+        game.getBatch().setProjectionMatrix(camera.combined);
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
