199430ac5c2a1893c936abe7df65cbe42a4f5a69
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-07-18 19:56:03 +0200

    refactor: - created class with methods with changing coordinates to pixels

---
+++ core/src/com/mygdx/chess/Cords.java
@@ -0,0 +1,12 @@
+package com.mygdx.chess;
+
+public class Cords {
+
+    static int xToPixels(int x) {
+        return x * 100;
+    }
+
+    static int yToPixels(int y) {
+        return y * 100;
+    }
+}
