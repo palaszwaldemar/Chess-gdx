35eff2ff1ccd3e19565cb0c9795313585f25307a
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-07-26 18:29:02 +0200

    refactor - changed creating list of remaining chess pieces: from three methods to only one.

--- core/src/com/mygdx/chess/server/PiecesFactory.java
+++ core/src/com/mygdx/chess/server/PiecesFactory.java
@@ -8,6 +8,9 @@ class PiecesFactory {
         List<ChessPiece> chessPieces = new ArrayList<>();
         int pawnsYPosition;
         int positionShift;
+        int[] xValuesRooks = {0, 7};
+        int[] xValuesKnights = {1, 6};
+        int[] xValuesRunners = {2, 5};
 
         if (color == ChessPieceColor.WHITE) {
             pawnsYPosition = 1;
@@ -20,9 +23,9 @@ class PiecesFactory {
         int chessPiecesYPosition = pawnsYPosition + positionShift;
 
         chessPieces.addAll(listOfPawns(color, pawnsYPosition));
-        chessPieces.addAll(listOfRooks(color, chessPiecesYPosition));
-        chessPieces.addAll(listOfKnight(color, chessPiecesYPosition));
-        chessPieces.addAll(listOfRunners(color, chessPiecesYPosition));
+        chessPieces.addAll(listOfRemainingChessPieces(ChessPieceType.ROOK, color, xValuesRooks, chessPiecesYPosition));
+        chessPieces.addAll(listOfRemainingChessPieces(ChessPieceType.KNIGHT, color, xValuesKnights, chessPiecesYPosition));
+        chessPieces.addAll(listOfRemainingChessPieces(ChessPieceType.RUNNER, color, xValuesRunners, chessPiecesYPosition));
         chessPieces.add(new ChessPiece(ChessPieceType.QUEEN, color, 3, chessPiecesYPosition));
         chessPieces.add(new ChessPiece(ChessPieceType.KING, color, 4, chessPiecesYPosition));
         return chessPieces;
@@ -38,42 +41,8 @@ class PiecesFactory {
         return chessPawns;
     }
 
-    private List<ChessPiece> listOfRooks(ChessPieceColor color, int y) {
-        List<ChessPiece> chessRooks = new ArrayList<>();
-        ChessPieceType type = ChessPieceType.ROOK;
-        int[] xValues = {0, 7};
-        for (int x : xValues) {
-            ChessPiece chessPiece = new ChessPiece(type, color, x, y);
-            chessRooks.add(chessPiece);
-        }
-        return chessRooks;
-    }
-
-    private List<ChessPiece> listOfKnight(ChessPieceColor color, int y) {
-        List<ChessPiece> chessKnights = new ArrayList<>();
-        ChessPieceType type = ChessPieceType.KNIGHT;
-        int[] xValues = {1, 6};
-        for (int x : xValues) {
-            ChessPiece chessPiece = new ChessPiece(type, color, x, y);
-            chessKnights.add(chessPiece);
-        }
-        return chessKnights;
-    }
-
-    private List<ChessPiece> listOfRunners(ChessPieceColor color, int y) {
-        List<ChessPiece> chessRunners = new ArrayList<>();
-        ChessPieceType type = ChessPieceType.RUNNER;
-        int[] xValues = {2, 5};
-        for (int x : xValues) {
-            ChessPiece chessPiece = new ChessPiece(type, color, x, y);
-            chessRunners.add(chessPiece);
-        }
-        return chessRunners;
-    }
-
-    private List<ChessPiece> listOfRemainingChessPieces(ChessPieceColor color, int[] xValues, int y) {// CHECK: 25.07.2023 chciałem zrobić wspólną metodę do generowania figur szachowych (oprócz pionków)
+    private List<ChessPiece> listOfRemainingChessPieces(ChessPieceType type, ChessPieceColor color, int[] xValues, int y) {
         List<ChessPiece> chessPieces = new ArrayList<>();
-        ChessPieceType type = ChessPieceType.RUNNER;
         for (int x : xValues) {
             ChessPiece chessPiece = new ChessPiece(type, color, x, y);
             chessPieces.add(chessPiece);
