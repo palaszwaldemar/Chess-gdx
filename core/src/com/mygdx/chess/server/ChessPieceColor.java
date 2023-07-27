34acf134eec7e8ce01ea47d5c9a7313a7742a115
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-07-27 17:35:09 +0200

    refactor: - refactor of creating list of chess pieces in 'PiecesFactory'

--- core/src/com/mygdx/chess/server/ChessPieceColor.java
+++ core/src/com/mygdx/chess/server/ChessPieceColor.java
@@ -1,16 +1,29 @@
 package com.mygdx.chess.server;
 
 enum ChessPieceColor {
-    BLACK("Black"),
-    WHITE("White");
+    BLACK("Black", 7, 6),
+    WHITE("White", 0, 1);
 
     private final String name;
+    private final int yFiguresPosition;
+    private final int yPawnsPosition;
 
-    ChessPieceColor(String name) {
+    ChessPieceColor(String name, int yFiguresPosition, int yPawnsPosition) {
         this.name = name;
+        this.yFiguresPosition = yFiguresPosition;
+        this.yPawnsPosition = yPawnsPosition;
+
     }
 
     String getName() {
         return name;
+    }
+
+    public int getYFiguresPosition() {
+        return yFiguresPosition;
+    }
+
+    public int getYPawnsPosition() {
+        return yPawnsPosition;
     }
 }
--- core/src/com/mygdx/chess/server/PiecesFactory.java
+++ core/src/com/mygdx/chess/server/PiecesFactory.java
@@ -4,48 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 class PiecesFactory {
+
+    ChessPieceType[] chessPieceTypes = {ChessPieceType.ROOK, ChessPieceType.KNIGHT, ChessPieceType.RUNNER,
+            ChessPieceType.QUEEN, ChessPieceType.KING, ChessPieceType.RUNNER,
+            ChessPieceType.KNIGHT, ChessPieceType.ROOK};
+
     List<ChessPiece> getChessPieces(ChessPieceColor color) {
         List<ChessPiece> chessPieces = new ArrayList<>();
-        int pawnsYPosition;
-        int positionShift;
-        int[] xValuesRooks = {0, 7};
-        int[] xValuesKnights = {1, 6};
-        int[] xValuesRunners = {2, 5};
-
-        if (color == ChessPieceColor.WHITE) {
-            pawnsYPosition = 1;
-            positionShift = -1;
-        } else {
-            pawnsYPosition = 6;
-            positionShift = 1;
-        }
-
-        int chessPiecesYPosition = pawnsYPosition + positionShift;
-
-        chessPieces.addAll(listOfPawns(color, pawnsYPosition));
-        chessPieces.addAll(listOfRemainingChessPieces(ChessPieceType.ROOK, color, xValuesRooks, chessPiecesYPosition));
-        chessPieces.addAll(listOfRemainingChessPieces(ChessPieceType.KNIGHT, color, xValuesKnights, chessPiecesYPosition));
-        chessPieces.addAll(listOfRemainingChessPieces(ChessPieceType.RUNNER, color, xValuesRunners, chessPiecesYPosition));
-        chessPieces.add(new ChessPiece(ChessPieceType.QUEEN, color, 3, chessPiecesYPosition));
-        chessPieces.add(new ChessPiece(ChessPieceType.KING, color, 4, chessPiecesYPosition));
+        chessPieces.addAll(listOfPawns(color));
+        chessPieces.addAll(listOfRemainingChessPieces(color));
         return chessPieces;
     }
 
-    private List<ChessPiece> listOfPawns(ChessPieceColor color, int y) {
+    private List<ChessPiece> listOfPawns(ChessPieceColor color) {
         List<ChessPiece> chessPawns = new ArrayList<>();
         ChessPieceType type = ChessPieceType.PAWN;
-        for (int x = 0; x < 8; x++) {
-            ChessPiece chessPiece = new ChessPiece(type, color, x, y);
+        for (int i = 0; i < 8; i++) {
+            ChessPiece chessPiece = new ChessPiece(type, color, i, color.getYPawnsPosition());
             chessPawns.add(chessPiece);
         }
         return chessPawns;
     }
 
-    private List<ChessPiece> listOfRemainingChessPieces(ChessPieceType type, ChessPieceColor color, int[] xValues, int y) {
+    private List<ChessPiece> listOfRemainingChessPieces(ChessPieceColor color) {
         List<ChessPiece> chessPieces = new ArrayList<>();
-        for (int x : xValues) {
-            ChessPiece chessPiece = new ChessPiece(type, color, x, y);
-            chessPieces.add(chessPiece);
+        for (int i = 0; i < 8; i++) {
+            chessPieces.add(new ChessPiece(chessPieceTypes[i], color, i, color.getYFiguresPosition()));
         }
         return chessPieces;
     }
