804ce82f45949fb7e3f4e56e09b3724c4bd40df4
Waldemar <palasz.waldemar0@gmail.com>
2023-10-09 11:42:49 +0200

    feature: - Pawn can start at the beginning with two fields

--- core/src/com/mygdx/chess/server/chessPieces/Pawn.java
+++ core/src/com/mygdx/chess/server/chessPieces/Pawn.java
@@ -4,6 +4,7 @@ import com.mygdx.chess.server.ChessPieceColor;
 import com.mygdx.chess.server.ChessPieceType;
 
 public class Pawn extends ChessPiece {
+    private int pawnMoves = 2;
 
     public Pawn(ChessPieceColor color, int x, int y) {
         super(ChessPieceType.PAWN, color, x, y);
@@ -11,10 +12,13 @@ public class Pawn extends ChessPiece {
 
     @Override
     public boolean isCorrectMovement(int xEndPosition, int yEndPosition) {
+        boolean correctMove;
         if (getColor().equals(ChessPieceColor.WHITE)) {
-            return getX() == xEndPosition && yEndPosition - getY() == 1;
+            correctMove = getX() == xEndPosition && yEndPosition - getY() <= pawnMoves && yEndPosition > getY();
         } else {
-            return getX() == xEndPosition && yEndPosition - getY() == -1;
+            correctMove = getX() == xEndPosition && yEndPosition - getY() >= -pawnMoves && yEndPosition < getY();
         }
+        if (correctMove) pawnMoves = 1;
+        return correctMove;
     }
 }
