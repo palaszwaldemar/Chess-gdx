05b0ab97053b50074837c586a9934246c5483c9b
Waldemar <palasz.waldemar0@gmail.com>
2023-10-10 21:47:02 +0200

    feature: - The readability of the "isCorrectMovement" method has been improved in the "king" class

--- core/src/com/mygdx/chess/server/chessPieces/King.java
+++ core/src/com/mygdx/chess/server/chessPieces/King.java
@@ -11,6 +11,8 @@ public class King extends ChessPiece {
 
     @Override
     public boolean isCorrectMovement(EndCordsVector endCordsVector) {
-        return Math.abs(getX() - endCordsVector.x) <= 1 && Math.abs(getY() - endCordsVector.y) <= 1;
+        int deltaX = Math.abs(getX() - endCordsVector.x);
+        int deltaY = Math.abs(getY() - endCordsVector.y);
+        return deltaX <= 1 && deltaY <= 1;
     }
 }
