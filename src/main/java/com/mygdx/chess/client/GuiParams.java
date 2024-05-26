e71c47af28d4381d1d479f3f6aab7d635802996b
Waldemar <palasz.waldemar0@gmail.com>
2024-05-26 12:22:24 +0200

    feature: added a method that calculates the screen size and checks whether the screen is vertical or horizontal

--- src/main/java/com/mygdx/chess/client/GuiParams.java
+++ src/main/java/com/mygdx/chess/client/GuiParams.java
@@ -3,7 +3,7 @@ package com.mygdx.chess.client;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class GuiParams {
-    public static final int WINDOW_SIZE = Lwjgl3ApplicationConfiguration.getDisplayMode().height - 100;
+    public static final int WINDOW_SIZE = getWindowSize() - 100;
     static final int CHESSBOARD_X_POSITION = 100;
     static final int CHESSBOARD_Y_POSITION = 100;
     static final int CHESS_PIECE_SIZE = 100;
@@ -17,4 +17,9 @@ public class GuiParams {
     static final int END_WINDOW_Y_POSITION = 500;
     static final int END_WINDOW_WIDTH = 200;
     static final int END_WINDOW_HEIGHT = 100;
+
+    private static int getWindowSize() {
+        return Math.min(Lwjgl3ApplicationConfiguration.getDisplayMode().height,
+            Lwjgl3ApplicationConfiguration.getDisplayMode().width);
+    }
 }
