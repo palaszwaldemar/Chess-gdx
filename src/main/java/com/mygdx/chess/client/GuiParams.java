0d543e20a1ab46cf9b824468eaa9dc4b19b1614a
Waldemar <palasz.waldemar0@gmail.com>
2024-05-26 13:58:04 +0200

    refactor: changed name of method

--- src/main/java/com/mygdx/chess/client/GuiParams.java
+++ src/main/java/com/mygdx/chess/client/GuiParams.java
@@ -3,7 +3,7 @@ package com.mygdx.chess.client;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class GuiParams {
-    public static final int WINDOW_SIZE = getWindowSize() - 150;
+    public static final int WINDOW_SIZE = setWindowSize() - 150;
     static final int CHESSBOARD_POSITION = WINDOW_SIZE / 10;
     static final int CHESSBOARD_SIZE = WINDOW_SIZE * 80 / 100;
     static final int CHESS_PIECE_SIZE = CHESSBOARD_SIZE / 8; //
@@ -16,7 +16,7 @@ public class GuiParams {
     static final int END_WINDOW_WIDTH = CHESS_PIECE_SIZE * 2;
     static final int END_WINDOW_HEIGHT = CHESS_PIECE_SIZE;
 
-    private static int getWindowSize() {
+    private static int setWindowSize() {
         return Math.min(Lwjgl3ApplicationConfiguration.getDisplayMode().height,
             Lwjgl3ApplicationConfiguration.getDisplayMode().width);
     }
